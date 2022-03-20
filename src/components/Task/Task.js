2b07fe84a376d83543f918e26328991d2fae344d
moretall <seongho4996@naver.com>
2022-03-20 14:41:18 +0900

    end

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -2,8 +2,14 @@ import * as React from 'react';
 
 export default function Task({ task: { id, title, state }, onArchiveTask, onPinTask }) {
     return (
-        <div className="list-item">
-            <input type="text" value={title} readOnly={true} />
+        <div className="title">
+            <input
+                type="text"
+                value={title}
+                readOnly={true}
+                placeholder="Input title"
+                style={{ background: 'red' }}
+            />
         </div>
     );
 }
